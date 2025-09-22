9a8310f41600eb5d2638d0da62d65be9fcc6ff86
Nguyễn Hoàng Long <huangiateku115@gmail.com>
2025-09-22 09:43:26 +0700

    testing

--- demo/src/main/java/com/example/demo/controller/TestController2.java
+++ demo/src/main/java/com/example/demo/controller/TestController2.java
@@ -8,6 +8,6 @@ public class TestController2 {
     // Ánh xạ GET cho đường dẫn gốc "/test"
     @GetMapping("/test2")
     public String test2() {
-        return "Test Controller 2 is working!";
+        return "Test Controller 2 is working! and git push is working";
     }
 }
