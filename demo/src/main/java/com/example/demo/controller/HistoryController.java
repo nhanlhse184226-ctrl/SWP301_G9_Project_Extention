3ee051c80753a4d7046a22ca88cdc45a6f05cb32
Lâm Hoàng Nhân <nhanlhse184226@fpt.edu.vn>
2025-09-29 10:16:08 +0700

    testingpush

--- demo/src/main/java/com/example/demo/controller/HistoryController.java
+++ demo/src/main/java/com/example/demo/controller/HistoryController.java
@@ -9,7 +9,7 @@ public class HistoryController {
     // Ánh xạ GET cho đường dẫn gốc "/test"
     @GetMapping("/history")
     public String test() {
-        return "History Controller is working!";
+        return "History Controller is workingiiii!";
     }
 
 }
